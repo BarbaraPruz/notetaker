275e0a6e70de4b66142548792bbb7107e83c2101
barbara prusiewicz <barbara.prusiewicz@gmail.com>
2020-04-12 15:41:46 -1000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import '@aws-amplify/ui/dist/style.css';
+
+import { withAuthenticator } from 'aws-amplify-react';
 
 function App() {
+  const [notes, setNotes] = useState (
+    [
+      {id: 1, note: "Hello World"}
+    ]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-1">Amplify Notetaker</h1>
+      <form className="mb3">
+        <input type="text" className="pa2 f4" placeholder="write your notes" />
+        <button type="submit" className="pa2 f4">Add Note</button>
+      </form>
+      <div>
+        {notes.map( item=> (
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1 f3">
+              {item.note}
+            </li>
+            <button className="bg-transparent bn f4">
+              <span>&times;</span>
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true});
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote {
+    onCreateNote {
+      id
+      Note
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote {
+    onUpdateNote {
+      id
+      Note
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote {
+    onDeleteNote {
+      id
+      Note
+    }
+  }
+`;
